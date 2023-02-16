2fdbbfb83b0e740e651b3a9aae650a38a680d0d7
Daniel Ogale <danielogale88@gmail.com>
2023-02-16 15:09:27 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-inputs.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+/**
+ * main - printing with the puts function
+ * Return: 0 (Success)
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Printing a line with the puts function
+ *
+ * return: 0 (Succes)
+ */
+int main(void)
+{
+puts("\"Programming is like buiding a multilingual puzzle");
+return (0);
+}
