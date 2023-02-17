77c72f735f07d39b322ff594eed4230d810e9f3f
Daniel Ogale <danielogale88@gmail.com>
2023-02-16 16:02:22 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - Printing a line with the puts function
  *
- * return: 0 (Succes)
+ * return: 0 (Success)
  */
-int main(void)
+int main()
 {
 puts("\"Programming is like buiding a multilingual puzzle");
 return (0);
